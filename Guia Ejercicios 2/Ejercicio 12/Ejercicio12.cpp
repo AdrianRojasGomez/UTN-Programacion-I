8e815d006c8aa77758e652d7b433116be8d8ca1a
Adrian Rojas <arojasgomez.93@gmail.com>
2024-09-21 13:22:08 -0300

    Ejercicio 12 de la Guia #2

--- Guia Ejercicios 2/Ejercicio 12/Ejercicio12.cpp
+++ Guia Ejercicios 2/Ejercicio 12/Ejercicio12.cpp
@@ -1,7 +1,41 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
 
- 	return 0;
+    int n1, n2, n3, medio;
+
+    cout << "Ingrese 3 numeros para calcular el del medio" << endl;
+
+    cin >> n1;
+    cin >> n2;
+    cin >> n3;
+
+	if (n1>n2 && n1>n3) {
+		if (n2>n3) {
+			medio = n2;
+		} else {
+			medio = n3;
+		}
+	} else {
+		if (n2>n1 && n2>n3) {
+			if (n1>n3) {
+				medio = n1;
+			} else {
+				medio = n3;
+			}
+		} else {
+			if (n1>n2) {
+				medio = n1;
+			} else {
+				medio = n2;
+			}
+		}
+	}
+
+
+    cout << "El numero medio es: " << medio << endl;
+
+    return 0;
 }
